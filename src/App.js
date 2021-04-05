f8b457d4b7b00a9bf08c5980c9abfa12dc81bd18
Syrine Joulak <syrinejoulak@gmail.com>
2021-04-05 16:59:10 +0200

    second

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Components/Navbar";
 import { movies } from "./Components/movies";
 import MovieList from "./Components/MovieList";
 import React, { Component } from "react";
+import Scroll from "./Components/Scroll";
 
 class App extends Component {
   constructor() {
@@ -42,7 +43,9 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar onSearch={this.onSearchChange} onRate={this.onRateChange} />
+        <Scroll>
         <MovieList movies={filtredMovies} addMovie={this.onAddChange} />
+        </Scroll>
       </div>
     );
   }
